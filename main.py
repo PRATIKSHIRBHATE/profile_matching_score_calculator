22a85096e3e35409809b81c72ef3d30898a10c41
Pratik Shirbhate <pratikshirbhate@gmail.com>
2023-05-06 11:43:00 +0530

    added new features

--- main.py
+++ main.py
@@ -25,21 +25,29 @@ def calculate_score():
     request_json["education"] = request_value_list[5]
     request_json["iit_nit"] = request_value_list[6]
     request_json["post_graduation"] = request_value_list[7]
-    request_json["avg_degree_percentage"] = float(request_value_list[8])
-    request_json["grad_year"] = int(request_value_list[9])
-    request_json["postgrad_year"] = int(request_value_list[10])
-    request_json["occupation"] = request_value_list[11]
-    request_json["experience"] = float(request_value_list[12])
-    request_json["monthly_salary"] = float(request_value_list[13])
-    request_json["work_from_home"] = request_value_list[14]
-    request_json["willing_to_contribute_equally_to_household_expense"] = request_value_list[15]
-    request_json["father_occupation"] = request_value_list[16]
-    request_json["mother_occupation"] = request_value_list[17]
-    request_json["relocate_to_yavatmal"] = request_value_list[18]
-    request_json["willing_to_live_with_inlaws"] = request_value_list[19]
-    request_json["non_vegiterian"] = request_value_list[20]
-    request_json["sleep_time"] = float(request_value_list[21])
-    request_json["wake_uptime"] = float(request_value_list[22])
+    request_json["grad_year"] = int(request_value_list[8])
+    request_json["postgrad_year"] = int(request_value_list[9])
+    request_json["ssc_percentage"] = float(request_value_list[10])
+    request_json["hsc_percentage"] = float(request_value_list[11])
+    request_json["avg_degree_percentage"] = float(request_value_list[12])
+    request_json["avg_post_grad_percentage"] = float(request_value_list[13])
+    request_json["occupation"] = request_value_list[14]
+    request_json["experience"] = float(request_value_list[15])
+    request_json["monthly_salary"] = float(request_value_list[16])
+    request_json["work_from_home"] = request_value_list[17]
+    request_json["willing_to_contribute_equally_to_household_expense"] = request_value_list[18]
+    request_json["father_occupation"] = request_value_list[19]
+    request_json["mother_occupation"] = request_value_list[20]
+    request_json["relocate_to_yavatmal"] = request_value_list[21]
+    request_json["willing_to_live_with_inlaws"] = request_value_list[22]
+    request_json["non_vegiterian"] = request_value_list[23]
+    request_json["sleep_time"] = float(request_value_list[24])
+    request_json["wake_uptime"] = float(request_value_list[25])
+    request_json["exp1"] = request_value_list[26]
+    request_json["exp2"] = request_value_list[27]
+    request_json["exp3"] = request_value_list[28]
+    request_json["exp4"] = request_value_list[29]
+    request_json["exp5"] = request_value_list[30]
 
     #request_json = json.dumps(request_json)
     #logging.info(request_json)
@@ -62,7 +70,10 @@ def calculate_score():
     education = request_json.get("education", "msc")
     iit_nit = request_json.get("iit_nit", "No")
     post_graduation = request_json.get("post_graduation", "No")
+    ssc_percentage = request_json.get("ssc_percentage", 0)
+    hsc_percentage = request_json.get("hsc_percentage", 0)
     avg_degree_percentage = request_json.get("avg_degree_percentage", 0)
+    avg_post_grad_percentage = request_json.get("avg_post_grad_percentage", 0)
 
     grad_year = request_json.get("grad_year", 9999)
     postgrad_year = request_json.get("postgrad_year", 9999)
@@ -99,7 +110,7 @@ def calculate_score():
     if weight_kg > 50 and weight_kg <70:
         total_score += 5
 
-    if hobby.lower() in ["reading", "playing chess", "cooking"]:
+    if hobby.lower() in ["reading", "playing chess", "cooking", "chess", "badminton", "swimming"]:
         total_score +=10
     elif hobby.lower() in ["watching tv", "social media", "netflix"]:
         total_score -=10
@@ -112,14 +123,20 @@ def calculate_score():
     if post_graduation.lower() in ["yes"] :
         total_score += 5
         
-    if avg_degree_percentage > 80:
+    if ssc_percentage >= 80:
+        total_score += 5
+    if hsc_percentage >= 80:
+        total_score += 5
+    if avg_degree_percentage >= 80:
         total_score += 10
+    if avg_post_grad_percentage >= 80:
+        total_score += 5
         
     # Penalize if there is gap in eduction until graduation
     if grad_year - birth_year > 22:
         total_score -= 10
 
-    if occupation.lower() in ["it", "software", "job", "engineer"] :
+    if occupation.lower() in ["it", "software", "job", "engineer", "data scientist"] :
         total_score += 5
 
     ideal_work_experience = current_year - grad_year - 2 # 2 for postgradution
@@ -130,7 +147,7 @@ def calculate_score():
         total_score += 5
 
     if work_from_home.lower() == 'yes':
-        total_score += 5
+        total_score += 15
 
     if willing_to_contribute_equally_to_household_expense.lower() == 'yes':
         total_score += 10
@@ -141,10 +158,10 @@ def calculate_score():
     if willing_to_live_with_inlaws.lower() == 'yes':
         total_score += 20
 
-    if father_occupation.lower() == "teacher":
+    if father_occupation.lower() in ["teacher", "professor", "retired teacher"]:
         total_score += 5
 
-    if mother_occupation.lower() == "teacher":
+    if mother_occupation.lower() in ["teacher", "professor", "retired teacher"]:
         total_score += 5
 
     if non_vegiterian.lower() == "yes":
@@ -155,17 +172,20 @@ def calculate_score():
     if wake_uptime > 7:
         total_score -= 10
 
+    percentage_score = round(100*total_score/180)
     # Perform some processing
-    greeting = "Hello, " + name + "! " + "Your profile matching score is " + str(total_score)
+    greeting = "Hello, " + name + "! " + "Your profile matching score is " + str(percentage_score)
     
     # Return the response to the user
     response = {}
     response["greeting"] = greeting
     request_json["total_score"] = total_score
+    request_json["percentage_score"] = percentage_score
     # logging.info(response)
     
     # Write the request-response to GCS
     # Create a client for interacting with Cloud Storage
+    
     client = storage.Client()
 
     # Select the bucket where you want to store the file
@@ -181,7 +201,7 @@ def calculate_score():
     # Write the JSON object to the Blob
     blob.upload_from_string(json_object)
 
-    return render_template('index.html', prediction_text='Greetings {}! your profile matching score is {}'.format(name, total_score))
+    return render_template('index.html', prediction_text='Greetings {}! your profile matching score is {} %'.format(name, percentage_score))
 
 
 if __name__ == "__main__":
